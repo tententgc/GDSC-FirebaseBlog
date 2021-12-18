0a7344f3a88eb5fd1f3ecf21835219745a2506ec
Thun (Mac Mini) <bsthun@gmail.com>
2021-12-18 13:32:01 +0700

    refactor to cdn

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# GDSC-FirebaseBlog
-google develop student clubs first workshop 
+# GDSC Firebase Blog
+**Build personal blog with Firebase** workshop event (Dec 19, 2021) website template.
+
+> Associated with **Google Developer Student Club: King Mongkut’s University of Technology Thonburi**
--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const categoryTitle = document.querySelectorAll('.category-title');
-const allCategoryPosts = document.querySelectorAll('.all');
+const categoryTitle = document.querySelectorAll (".category-title");
+const allCategoryPosts = document.querySelectorAll (".all");
 
-for(let i = 0; i < categoryTitle.length; i++){
-    categoryTitle[i].addEventListener('click', filterPosts.bind(this, categoryTitle[i]));
+for (let i = 0; i < categoryTitle.length; i++) {
+    categoryTitle[i].addEventListener ("click", filterPosts.bind (this, categoryTitle[i]));
 }
 
-function filterPosts(item){
-    changeActivePosition(item);
-    for(let i = 0; i < allCategoryPosts.length; i++){
-        if(allCategoryPosts[i].classList.contains(item.attributes.id.value)){
+function filterPosts (item) {
+    changeActivePosition (item);
+    for (let i = 0; i < allCategoryPosts.length; i++) {
+        if (allCategoryPosts[i].classList.contains (item.attributes.id.value)) {
             allCategoryPosts[i].style.display = "block";
         } else {
             allCategoryPosts[i].style.display = "none";
@@ -16,9 +16,9 @@ function filterPosts(item){
     }
 }
 
-function changeActivePosition(activeItem){
-    for(let i = 0; i < categoryTitle.length; i++){
-        categoryTitle[i].classList.remove('active');
+function changeActivePosition (activeItem) {
+    for (let i = 0; i < categoryTitle.length; i++) {
+        categoryTitle[i].classList.remove ("active");
     }
-    activeItem.classList.add('active');
-};
+    activeItem.classList.add ("active");
+}
